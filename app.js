e3b347bfa837bef78584fba3267901a2d3ce350a
Nelly Mahecha <nejumava@gmail.com>
2020-01-29 08:40:32 -0500

    Solucion a reto

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  var nombre = req.query.nombre;
+  if(nombre == null || nombre == ''){
+      nombre = "desconocido";
+  }
+  res.send('<h1>Hola ' + nombre + '!</h1>');
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
